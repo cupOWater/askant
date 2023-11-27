6861330ce4a70bf4d216b360d021c675858d5f79
Tony <fc0302@naver.com>
2023-11-27 21:55:33 +0700

    done for designing header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ForumHome from "./components/ForumHome";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <div className="App">
         <Routes>
           <Route exact path="/" element={<ForumHome />} />
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,35 +1,23 @@
+import logo from "../assets/images/logo.png"
 import "../assets/styles/header.css";
 
 function Header() {
     return (
         <header className="header">
-            <div className="headerTop">
-                <nav>
-                    <ul className="mb-0 pb-0">
-                        <li><a href="#">Save More on App</a></li>
-                        <li><a href="/search">Sell On Lazada</a></li>
-                        <li><a href="#">Lazada Care</a></li>
-                        <li></li>
-                    </ul>
-                </nav>
-            </div>
-
-            <nav className="navbar navbar-expand-lg bg-body-tertiary my-0">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="/">
-                        <img src={logo} className="headerImage ms-2"></img>
-                    </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#lazadaHeader" aria-controls="lazadaHeader" >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="d-flex float-right" id="lazadaHeader">
-                        <a href="/cart">
-                            <button className="cartBtn me-3">
-
-                            </button>
-                        </a>
-                </div>
-                </div>
+            <nav>
+                <ul className="mb-0 pb-0">  
+                    <div className="headerLeft">
+                        <li className="headerLeftItem">Shopping</li>
+                        <li className="headerLeftItem">Forum</li>
+                    </div>
+                    <div className="headerCenter">
+                        <li><img src={logo} className="headerImage ms-2"></img></li>
+                    </div>
+                    <div className="headerRight">
+                        <li className="headerRightItem">Sign up</li>
+                        <li className="headerRightItem"><button type="button" class="btn btn-warning">Log in</button></li>
+                    </div>
+                </ul>
             </nav>
         </header>
     )
