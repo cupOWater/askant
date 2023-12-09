d3860c5eac8ee645b7c7db0466b9bc7375bc52db
cupOWater <something2805@gmail.com>
2023-12-09 22:35:43 +0700

    Scrape considers Pagination

--- backend/scrape.js
+++ backend/scrape.js
@@ -1,29 +1,24 @@
 const puppeteer = require("puppeteer");
 
 class ScrapeDomain {
-    constructor(domainName, mainQr, nameQr, priceQr, imgQr, antUrls, tankUrl, equipUrl) {
+    constructor(domainName, mainQr, nameQr, priceQr, imgQr, nextQr, urls) {
         this.domainName = domainName;
         this.mainQr = mainQr;
         this.nameQr = nameQr;
         this.priceQr = priceQr;
         this.imgQr = imgQr;
-        this.antUrls = antUrls;
-        this.tankUrl = tankUrl;
-        this.equipUrl = equipUrl;
+        this.nextQr = nextQr;
+        this.urls = urls;
     }
 }
 
 const scrapeDomains = [
-    new ScrapeDomain("HT Exotic Zone", ".ant-single-product", ".product-title", ".price", "img",
+    new ScrapeDomain("HT Exotic Zone", ".ant-single-product", ".product-title", ".price", "img", "a[title='»']",
         [
-            "https://hactexoticzone.com/kien-an-thit",
-            "https://hactexoticzone.com/kien-an-hat"
-        ],
-        [
-            "https://hactexoticzone.com/tank-nuoi-kien"
-        ],
-        [
-            "https://hactexoticzone.com/vat-dung-nuoi-kien"
+            { url: "https://hactexoticzone.com/kien-an-thit", cat: "Ant" },
+            { url: "https://hactexoticzone.com/kien-an-hat", cat: "Ant" },
+            { url: "https://hactexoticzone.com/tank-nuoi-kien", cat: "Tank" },
+            { url: "https://hactexoticzone.com/vat-dung-nuoi-kien", cat: "Supply" }
         ]
     )
 ]
@@ -32,36 +27,51 @@ const scrapeDomains = [
 // Store as {productName, productPrice, productImgSrc, Domain, Category}
 async function scrapeWebsite(domain) {
     const browser = await puppeteer.launch({ headless: "false", defaultViewport: null });
-    const page = await browser.newPage(); 
+    const page = await browser.newPage();
     const result = []
 
-    antUrls = domain.antUrls;
-    for (let i = 0; i < antUrls.length; i++) {
-        await page.goto(antUrls[i]);
-        await page.waitForSelector(domain.mainQr);
-        result.push(...await page.$$eval(domain.mainQr, (elements, nameQr, priceQr, imgQr, dmName) => {
-            return elements.map(element => {
-                const productName = element.querySelector(nameQr).textContent;
-                const productPrice = element.querySelector(priceQr).textContent;
-                const img = element.querySelector(imgQr)
-                const productImg = img.getAttribute("data-lazyload") || img.getAttribute("src");
-                return { name: productName, price: productPrice, img: productImg, domain: dmName, category: "Ant" };
-            })
-        }, domain.nameQr, domain.priceQr, domain.imgQr, domain.domainName));
-    }
+    urls = domain.urls;
+    for (let i = 0; i < urls.length; i++) {
+        await page.goto(urls[i].url);
+        let nextLink;
 
+        do {
+            await page.waitForSelector(domain.mainQr);
+    
+            let nextButtonNode = await page.$(domain.nextQr)
+            if (nextButtonNode) {
+                nextLink = await nextButtonNode.evaluate(e => e.getAttribute("href"));
+            }
+
+            result.push(...await page.$$eval(domain.mainQr, (elements, nameQr, priceQr, imgQr, dmName, category) => {
+                return elements.map(element => {
+                    const productName = element.querySelector(nameQr).textContent;
+                    const productPrice = element.querySelector(priceQr).textContent;
+                    const img = element.querySelector(imgQr)
+                    const productImg = img.getAttribute("data-lazyload") || img.getAttribute("src");
+                    return { name: productName, price: productPrice, img: productImg, domain: dmName, category: category };
+                })
+            }, domain.nameQr, domain.priceQr, domain.imgQr, domain.domainName, urls[i].cat));
+ 
+            if (nextLink && nextLink !== "#") {
+                await page.click(domain.nextQr);
+            }
+        }
+        while (nextLink && nextLink !== "#")
+
+
+    }
 
     await browser.close()
     return result;
 }
 
-scrapeWebsite(scrapeDomains[0])
-    .then(result => {
+try {
+    scrapeWebsite(scrapeDomains[0]).then(result => {
         result.map(r => console.log(r));
     })
-    .catch(err => {
-        console.log("DAMN!");
-        console.log(err)
-    })
+} catch (err) {
+    console.log(err);
+}
 
 
