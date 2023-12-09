7de130d0bc5e1bafca122ac5ba128b214793a669
cupOWater <something2805@gmail.com>
2023-12-09 17:34:32 +0700

    Domain Class for easier scraping

--- backend/scrape.js
+++ backend/scrape.js
@@ -1,36 +1,67 @@
 const puppeteer = require("puppeteer");
 
-async function scrapeWebsite(url, selector) {
-    const browser = await puppeteer.launch({headless: "false", defaultViewport: null});
-    const page = await browser.newPage();
-
-    await page.goto(url);
-    await page.waitForSelector(selector);
-    
-    const elementText = await page.$$eval(selector, elements => {
-        return elements.map(element => {
-            const productName = element.querySelector(".product-title").textContent;
-            const productPrice = element.querySelector(".price").textContent;
-            const productImg = element.querySelector("img").src;
-            return {name: productName, price: productPrice, img: productImg};
-        })
-    });
-
-    await browser.close()
-
-    return elementText;
+class ScrapeDomain {
+    constructor(domainName, mainQr, nameQr, priceQr, imgQr, antUrls, tankUrl, equipUrl) {
+        this.domainName = domainName;
+        this.mainQr = mainQr;
+        this.nameQr = nameQr;
+        this.priceQr = priceQr;
+        this.imgQr = imgQr;
+        this.antUrls = antUrls;
+        this.tankUrl = tankUrl;
+        this.equipUrl = equipUrl;
+    }
 }
 
-const websiteURL = "https://hactexoticzone.com/tank-nuoi-kien";
-const elementSelector = ".ant-single-product";
-const products = []
-scrapeWebsite(websiteURL, elementSelector)
+const scrapeDomains = [
+    new ScrapeDomain("HT Exotic Zone", ".ant-single-product", ".product-title", ".price", "img",
+        [
+            "https://hactexoticzone.com/kien-an-thit",
+            "https://hactexoticzone.com/kien-an-hat"
+        ],
+        [
+            "https://hactexoticzone.com/tank-nuoi-kien"
+        ],
+        [
+            "https://hactexoticzone.com/vat-dung-nuoi-kien"
+        ]
+    )
+]
+
+
+// Store as {productName, productPrice, productImgSrc, Domain, Category}
+async function scrapeWebsite(domain) {
+    const browser = await puppeteer.launch({ headless: "false", defaultViewport: null });
+    const page = await browser.newPage(); 
+    const result = []
+
+    antUrls = domain.antUrls;
+    for (let i = 0; i < antUrls.length; i++) {
+        await page.goto(antUrls[i]);
+        await page.waitForSelector(domain.mainQr);
+        result.push(...await page.$$eval(domain.mainQr, (elements, nameQr, priceQr, imgQr, dmName) => {
+            return elements.map(element => {
+                const productName = element.querySelector(nameQr).textContent;
+                const productPrice = element.querySelector(priceQr).textContent;
+                const img = element.querySelector(imgQr)
+                const productImg = img.getAttribute("data-lazyload") || img.getAttribute("src");
+                return { name: productName, price: productPrice, img: productImg, domain: dmName, category: "Ant" };
+            })
+        }, domain.nameQr, domain.priceQr, domain.imgQr, domain.domainName));
+    }
+
+
+    await browser.close()
+    return result;
+}
+
+scrapeWebsite(scrapeDomains[0])
     .then(result => {
-        result.map(e => {
-            console.log(e);
-            return;
-        })
+        result.map(r => console.log(r));
     })
-    .catch(error => {
-        console.error("error: ", error)
+    .catch(err => {
+        console.log("DAMN!");
+        console.log(err)
     })
+
+
