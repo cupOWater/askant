dc53a677250903fc77aa7d109257116e3b0d2785
PaleVoid <70835432+PaleVoid@users.noreply.github.com>
2023-12-17 11:29:51 +0700

    remove ChatBot Env
    Remove ChatBot env File

--- frontend/src/Chatbot/autoBot.js
+++ frontend/src/Chatbot/autoBot.js
@@ -1,7 +1,5 @@
 import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
 
-const APP_ID = process.env.REACT_APP_APP_ID;
-
 const Autobot = () => {
     Kommunicate.init('16f2badde92dcc243574b09a7376a2e2c', {
         automaticChatOpenOnNavigation: true,
