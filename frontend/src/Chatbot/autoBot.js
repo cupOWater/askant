76c0ece22ff24609dca951a32720b431d51ea848
PaleVoid <70835432+PaleVoid@users.noreply.github.com>
2023-12-30 18:45:59 +0700

    Update autoBot.js

--- frontend/src/Chatbot/autoBot.js
+++ frontend/src/Chatbot/autoBot.js
@@ -1,11 +1,47 @@
-import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
+import React, { useEffect } from 'react';
 
-const Autobot = () => {
-    Kommunicate.init('16f2badde92dcc243574b09a7376a2e2c', {
-        automaticChatOpenOnNavigation: true,
-        popupWidget: true
-        }
-    );
-}
+const AutoBot = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://cdn.botpress.cloud/webchat/v1/inject.js';
+    script.async = true;
+    document.body.appendChild(script);
 
-export default Autobot;
+    script.onload = () => {
+      window.botpressWebChat.init({
+        composerPlaceholder: "Chat with AskAnt",
+        botConversationDescription: "All About Ants",
+        botId: "2047fd74-3e58-4c67-92a3-458b680e509e",
+        hostUrl: "https://cdn.botpress.cloud/webchat/v1",
+        messagingUrl: "https://messaging.botpress.cloud",
+        clientId: "2047fd74-3e58-4c67-92a3-458b680e509e",
+        webhookId: "b94314f7-ad2c-40a5-88b9-ffb8571a0ee5",
+        lazySocket: true,
+        themeName: "prism",
+        botName: "AskAnt",
+        frontendVersion: "v1",
+        useSessionStorage: true,
+        showPoweredBy: true,
+        theme: "prism",
+        themeColor: "#B00C0C"
+      });
+    };
+  }, []);
+
+  const handleButtonClick = () => {
+    if (window.botpressWebChat) {
+      window.botpressWebChat.sendPayload({ type: 'text', text: '🎄 Merry Christmas, Chatbot Users! ✨' });
+    }
+  };
+
+  return (
+    <div>
+      <h1 onClick={handleButtonClick}>Click me!</h1>
+      <button id="sayButton" onClick={handleButtonClick}>
+        Send message to chatbot
+      </button>
+    </div>
+  );
+};
+
+export default AutoBot;
