2aa7645608b4fcf0af0d2d5451ac28b7a2c9aa94
cupOWater <something2805@gmail.com>
2024-01-07 18:47:22 +0700

    change some footer display

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,31 +6,31 @@ import youtube from "../assets/images/youtube.png"
 import mail from "../assets/images/mail.png"
 import "../assets/styles/footer.css";
 
-const Footer = () =>{
+const Footer = () => {
     return (
-        <footer className="footer fixed-bottom  bg-light mt-10">
+        <footer className="footer bg-light mt-3">
             <div className="container">
                 <div className="row">
-                    
                     <div className="col-md-6">
-                    <h5>Contact</h5>
-                    <p><img src ={phone} class ="icon-left" alt="phone"/>+84 901234567 </p>
-                    <p><img src ={mail} class ="icon-left"/> askant@gmail.com</p>
+                        <h5>Contact</h5>
+                        <p>
+                            <span className="me-4"><img src={phone} class="icon-left" alt="phone" />+84 901234567</span>
+                            <span><img src={mail} class="icon-left" /> askant@gmail.com</span>
+                        </p>
                     </div>
-                
-                
+
                     <div className="col-md-6">
                         <h5>Follow Us</h5>
-                            <ul>
-                                <li class="footer-right-item"><img src={facebook} class="icon-right" alt="facebook"/></li>
-                                <li class="footer-right-item"><img src={instagram} class="icon-right" alt="instagram"/></li>
-                                <li class="footer-right-item"><img src={github} class="icon-right" alt="github"/></li>
-                                <li class="footer-right-item"><img src={youtube} class="icon-right" alt="youtube"/></li>
-                            </ul>
+                        <ul>
+                            <li class="footer-right-item"><img src={facebook} class="icon-right" alt="facebook" /></li>
+                            <li class="footer-right-item"><img src={instagram} class="icon-right" alt="instagram" /></li>
+                            <li class="footer-right-item"><img src={github} class="icon-right" alt="github" /></li>
+                            <li class="footer-right-item"><img src={youtube} class="icon-right" alt="youtube" /></li>
+                        </ul>
                     </div>
                 </div>
             </div>
-        </footer>
+        </footer >
     )
 }
 export default Footer
